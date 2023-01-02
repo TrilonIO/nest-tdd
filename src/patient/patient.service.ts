da12300fc84e88343f693a3f562dc141ec9cb953
Thiago Martins <thiago_valentim1@hotmail.com>
2023-01-02 14:59:30 -0300

    refactor(patient): add input and output interfaces

--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PatientModel } from './patient.model';
+
+export interface PatientInput {
+  name: string;
+}
 
 @Injectable()
 export class PatientService {
-  register(patientData: any) {
+  register(patientData: PatientInput): PatientModel {
     return {
       id: 1,
       name: patientData.name,
