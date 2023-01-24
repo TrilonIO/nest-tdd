c88fa51b2e280c859ddb3072d9b57085b12815b1
Thiago Martins <thiago_valentim1@hotmail.com>
2023-01-24 16:14:39 -0300

    refactor: save patients within patients controller

--- src/patients/patients.controller.ts
+++ src/patients/patients.controller.ts
@@ -3,14 +3,19 @@ import { RegisterPatientDto } from './dtos';
 
 @Controller('patients')
 export class PatientsController {
+  private readonly patients: any[] = [];
   @Post()
   public async registerPatient(
     @Body() registerPatientInput: RegisterPatientDto,
   ) {
-    return {
+    const newPatient = {
       id: 1,
       name: registerPatientInput.name,
       email: registerPatientInput.email,
     };
+
+    this.patients.push(newPatient);
+
+    return newPatient;
   }
 }
--- test/patients.e2e-spec.ts
+++ test/patients.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { setupApp } from '../src/app.setup';
