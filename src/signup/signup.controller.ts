f1a1d18ae1e1d9e8521effab19942d3bcb592b63
Thiago Martins <thiago_valentim1@hotmail.com>
2023-01-25 13:53:11 -0300

    refactor(signup): refactor signup to use dto

--- src/signup/signup.controller.ts
+++ src/signup/signup.controller.ts
@@ -1,9 +1,10 @@
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { SignupDto } from './signup.dto';
 
 @Controller('signup')
 export class SignupController {
   @Post()
-  public async signup(@Body() signupDto: any) {
+  public async signup(@Body() signupDto: SignupDto) {
     if (!signupDto.name) {
       throw new BadRequestException({
         errors: ['name is required'],
---
+++ src/signup/signup.dto.ts
@@ -0,0 +1,6 @@
+export class SignupDto {
+  public name: string;
+  public email: string;
+  public password: string;
+  public passwordConfirmation: string;
+}
